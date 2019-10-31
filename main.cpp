436a02be1ba3080397592b6b4779cdbb4e64931a
xulizi <lizi_xu@foxmail.com>
2019-10-31 18:32:24 -0400

    Introduction of Shader

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <math.h>
+
 #include <iostream>
 
 #include "shader.h"
@@ -14,15 +16,14 @@
 
 
 float vertices[] = {
-    0.5f, 0.5f, 0.0f,   // 右上角
-    0.5f, -0.5f, 0.0f,  // 右下角
-    -0.5f, -0.5f, 0.0f, // 左下角
-    -0.5f, 0.5f, 0.0f   // 左上角
-};
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+}; 
 
-unsigned int indices[] = { // 注意索引从0开始!
-    0, 1, 3, // 第一个三角形
-    1, 2, 3  // 第二个三角形
+unsigned int indices[] = {
+    0, 1, 2,
 };
 
 
@@ -49,14 +50,26 @@ void ProcessInput(GLFWwindow *window)
 // render
 void DoRender(uint& object, uint& shaderProgram)
 {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Note that finding the uniform location does not require you to use the shader program first.
+//    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 
     glUseProgram(shaderProgram);
+    
+//    float timeValue = glfwGetTime();
+//    float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+//    float redValue = (cos(timeValue) / 2.0f) + 0.5f;
+//    float blueValue = (sin(timeValue + M_PI/4.0f) / 2.0f) + 0.5f;
+    // Note that updating a uniform does require you to first use the program (by calling glUseProgram),
+    // because it sets the uniform on the currently active shader program.
+//    glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+
 //    glBindVertexArray(object);
 //    glDrawArrays(GL_TRIANGLES, 0, 3);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, object);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 }
 
 
@@ -119,13 +132,18 @@ int main(int argc, const char * argv[])
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // step 4: set position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     
-
+    // step 5: set color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
     // 设置绘制模式
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    
+    // main loop
     while(!glfwWindowShouldClose(window)) {
         // input
         ProcessInput(window);
--- shader/simple.fs
+++ shader/simple.fs
@@ -1,8 +1,12 @@
 #version 330 core
 
+//uniform vec4 ourColor;  // we set this variable in the OpenGL code.
+
+in vec4 vertexColor;
+
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vertexColor;
 }
